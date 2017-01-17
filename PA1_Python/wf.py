0040b898ccd63a0683c365038001f55eebb76dc3
PrinceKannah <princekannah@onyxnode52.boisestate.edu>
2017-01-17 11:35:52 -0700

    Update wf.py to read from file instead of std-in

--- PA1_Python/wf.py
+++ PA1_Python/wf.py
@@ -1,8 +1,15 @@
 # Import sys to read from std-in, re for regular expression parsing
 import sys, re
 
+if len(sys.argv) <=1:
+  print("A file to parse must be specified")
+  print("Usage: \n python wf.py <file>")
+  sys.exit(1)
+
+f = open(sys.argv[1], 'r')  # Open the file in read only mode
+print("Statistics for " + sys.argv[1])
 wordFreq = {}
-for line in sys.stdin:  # Read line by line from std-in
+for line in f:  # Read line by line from std-in
     cleanLine = re.sub(r'[^\w\s]', '', line)  # Clean up the line
     wordList = cleanLine.split()  # Split each word separated by space; place each word in the words list
     for word in wordList:  # Go through the word list to look at each word
@@ -11,6 +18,7 @@ for line in sys.stdin:  # Read line by line from std-in
             wordFreq[token] = 1
         else:
             wordFreq[token] += 1
-
+f.close()  # Close the file
+# Print the stats to standard out
 for key, value in sorted(wordFreq.items(), key=lambda y:y[1], reverse=True):
-    print("%s:%s" % (key, value))
+    print("%s %s" % (key, value))
